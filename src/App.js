6fad76d8db38d4c0f61b82970471bddf3f689a53
haneolyPa <nomakan@naver.com>
2021-10-25 16:36:39 +0900

    03 컴포넌트, props 개념잡기

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react'
+import Food from './Food'
 function App() {
-  return <div>Hello!!!</div>;
+  return (
+  <div>
+    <h1>Hello</h1>
+    <Food fav="Kimchi"/>
+    <Food fav="ramen"/>
+    <Food fav="samgiopsal"/>
+    <Food fav="chukumi"/>
+  </div>);
 }
 
 export default App;
---
+++ src/Food.js
@@ -0,0 +1,6 @@
+import React from 'react'
+function Food({fav}) {
+    return (<h1>I love {fav}.</h1>);
+}
+
+export default Food;
