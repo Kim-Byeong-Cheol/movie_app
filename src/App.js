1e34c5ed01bf62dee7125ce90e85d9e9ee14fd5d
haneolyPa <nomakan@naver.com>
2021-10-27 18:24:16 +0900

    05-2 숫자 증감 기능을 제대로 만들어 보기 액션 04: setState() 함수로 count state 변경하기

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ class App extends React.Component {
   };
 
   add = () => {
-    this.state.count = 1;
+    this.setState({ count: 1});
   }
 
   minus = () => {
-    this.state.count = -1;
+    this.setState({ count: -1});
   }
 
   render() {
     return (
       <div>
         <h1>The number is {this.state.count}</h1>
-        <button>Add</button>
-        <button>Minus</button>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
       </div>
     )
   }  
