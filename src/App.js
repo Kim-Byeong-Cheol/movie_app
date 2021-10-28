5dbda465ffce47b7089f9cf8b6434124af8d0a41
haneolyPa <nomakan@naver.com>
2021-10-28 14:47:09 +0900

    05-4 영화 앱 만들기 워밍업
    액션 01: App 컴포넌트 비우기

--- src/App.js
+++ src/App.js
@@ -1,48 +1,8 @@
 import React from 'react';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    console.log('hello.');
-  }
-
-  state = {
-    count: 0,
-  };
-
-  add = () => {
-    this.setState(current => ({
-      count: current.count + 1
-    }));
-  }
-
-  minus = () => {
-    this.setState(current => ({
-      count: current.count - 1
-    }));
-  }
-
   render() {
-    console.log("I'm render.");
-    return (
-      <div>
-        <h1>The number is {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    )
-  }
-  
-  componentDidMount() {
-    console.log("I'm componentDidMount.");
-  }
-
-  componentDidUpdate() {
-    console.log("I'm componentDidUpdate.");
-  }
-
-  componentWillUnmount() {
-    console.log("Goodbye, cruel world.");
+    return <div />;
   }
 }
 
