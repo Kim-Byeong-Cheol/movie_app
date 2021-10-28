78cb6952d6ccb0891623a2ea14f79adbbb0f1521
haneolyPa <nomakan@naver.com>
2021-10-28 15:01:45 +0900

    05-4 영화 앱 만들기 워밍업
    액션 05: 영화 데이터를 어딩에 저장할까?

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ class App extends React.Component {
     const { isLoading } = this.state;
     return <div>{isLoading ? 'Loading...': 'We ar ready'}</div>;
   }
+
   componentDidMount() {
+    //영화 데이터 로딩!
+    
     setTimeout(() => {
       this.setState({isLoading: false});
     }, 6000);
