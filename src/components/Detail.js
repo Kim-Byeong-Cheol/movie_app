eee42a9ae5e3ca075f3c35549b406499469fc203
haneolyPa <nomakan@naver.com>
2021-11-03 20:00:19 +0900

    08-5 리다이렉트 기능 만들어 보기
    액션 03: push() 함수 사용하기

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,6 +3,10 @@ import React from "react";
 class Detail extends React.Component {
     componentDidMount() {
         const { location, history } = this.props;
+
+        if(location.state === undefined) {
+            history.push('/');
+        }
     }
 
     render() {
